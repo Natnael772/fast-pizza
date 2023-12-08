272aa4498c3404cc88b0f57f17ad56e2699d3965
natnael772 <natnaeldeyas0@gmail.com>
2023-12-08 23:33:20 +0300

    render error based on the error message coming from useRouteError hook

--- src/ui/Error.jsx
+++ src/ui/Error.jsx
@@ -1,10 +1,16 @@
 import { useNavigate, useRouteError } from "react-router-dom";
 
 function Error() {
+  const navigate = useNavigate();
+
+  //useRouteError returns anything thrown during an action, loader, or rendering.
+  const error = useRouteError();
+  console.log(error.message);
+
   return (
     <div>
       <h1>Something went wrong 😢</h1>
-
+      <p>{error.data || error.message}</p>
       <button onClick={() => navigate(-1)}>&larr; Go back</button>
     </div>
   );

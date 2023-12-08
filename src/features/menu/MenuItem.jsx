525f6e12793833247c0c55b91c84e2bc2cfaf4ff
natnael772 <natnaeldeyas0@gmail.com>
2023-12-08 22:57:50 +0300

    feat: add menuitem component to display menu item

---
+++ src/features/menu/MenuItem.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+import { formatCurrency } from "../../utils/helpers";
+
+function MenuItem({ pizza }) {
+  const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+
+  return (
+    <Link to={`/cart/${id}`}>
+      <img src={imageUrl} alt={name} />
+      <div>
+        <p>{name}</p>
+        <p>{ingredients.join(", ")}</p>
+        <div>
+          {!soldOut ? <p>{formatCurrency(unitPrice)}</p> : <p>Sold out</p>}
+        </div>
+      </div>
+    </Link>
+  );
+}
+
+export default MenuItem;

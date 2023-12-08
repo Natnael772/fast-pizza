88ac7cd3236809275000643d1bcb9340f586aa8a
natnael772 <natnaeldeyas0@gmail.com>
2023-12-08 22:38:50 +0300

    feat: App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./ui/Home";
+import Menu, { loader as menuLoader } from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import Order, { loader as orderLoader } from "./features/order/Order";
+import CreateOrder, {
+  action as createOrderAction,
+} from "./features/order/CreateOrder";
+import AppLayout from "./ui/AppLayout";
+import Error from "./ui/Error";
+
+// createBrowserRouter is used for data fetching/loading with react router
+const router = createBrowserRouter([]);
+
 function App() {
-  return <></>;
+  return <RouterProvider router={router}> </RouterProvider>;
 }
 
 export default App;

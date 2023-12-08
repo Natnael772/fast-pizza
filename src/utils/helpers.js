cf8b9b7fb4e7f2babb881d327e14cfceb59098e0
natnael772 <natnaeldeyas0@gmail.com>
2023-12-08 23:00:10 +0300

    feat: add formatdate util function to format date string

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -4,3 +4,12 @@ export function formatCurrency(value) {
     currency: "EUR",
   }).format(value);
 }
+
+export function formatDate(dateStr) {
+  return new Intl.DateTimeFormat("en", {
+    day: "numeric",
+    month: "short",
+    hour: "2-digit",
+    minute: "2-digit",
+  }).format(new Date(dateStr));
+}

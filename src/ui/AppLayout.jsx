ff6e329f77e3a935f872eb441d4f30dcd6f2c7fc
natnael772 <natnaeldeyas0@gmail.com>
2023-12-08 22:35:41 +0300

    feat: render loader component during loading state

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -5,16 +5,23 @@ import { Outlet, useNavigation } from "react-router-dom";
 import Loader from "./Loader";
 
 export default function AppLayout() {
+  const navigation = useNavigation();
+  console.log(navigation);
+
+  const isLoading = navigation.state === "loading";
+  console.log(isLoading);
+
   return (
     <div className="layout">
       <Header />
-
+      {isLoading && <Loader />}
       <main>
         <h1>content</h1>
 
         {/* render child routes  */}
         <Outlet />
       </main>
+      <CartOverview />
     </div>
   );
 }

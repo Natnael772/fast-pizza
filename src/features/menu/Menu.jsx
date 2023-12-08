638aeeb314482d48e6fc306e695e7438ebd60c21
natnael772 <natnaeldeyas0@gmail.com>
2023-12-08 22:54:27 +0300

    feat: add loader function to fetch all menus to later display it in menu component

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,5 +1,16 @@
+import { useLoaderData } from "react-router-dom";
+import { getMenu } from "../../services/apiRestaurant";
+import MenuItem from "./MenuItem";
+
 function Menu() {
-  return <h1>Menu</h1>;
+  return <></>;
+}
+
+//render as you fetch strategy
+//rendering and fetching happens at the same time here
+export async function loader() {
+  const menu = await getMenu();
+  return menu;
 }
 
 export default Menu;

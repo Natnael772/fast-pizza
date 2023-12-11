54f44a98d652eeffde484c67b2432fbb495ff8f7
natnael772 <natnaeldeyas0@gmail.com>
2023-12-11 14:08:35 +0300

    style: add styling to header

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
+import Username from "../features/user/Username";
 
 export default function Header() {
   return (
-    <header>
-      <Link to="/">Fast Pizza Co.</Link>
+    <header className="font-roboto flex items-center  justify-between border-b border-stone-200 bg-yellow-500 px-4  py-3 font-medium uppercase sm:px-6">
+      <Link to="/" className="tracking-normal">
+        Fast Pizza Co.
+      </Link>
       <SearchOrder />
+      <Username />
     </header>
   );
 }

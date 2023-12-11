46aa41b8e5293487245f51e7c4b5b2a026bb347a
natnael772 <natnaeldeyas0@gmail.com>
2023-12-11 14:10:59 +0300

    style: add styling

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
+// div  text-stone-200
 
 function CartOverview() {
   return (
-    <div>
-      <p>
+    <div className="flex items-center justify-between gap-10 bg-stone-800 p-4 text-sm uppercase text-stone-200 sm:px-6 md:text-base">
+      <p className="space-x-4 font-semibold text-stone-300 sm:space-x-6 md:text-base ">
+        {/* <p className="space-x-4 font-semibold text-orange-900 sm:text-green-500  md:text-yellow-500 lg:text-red-500"> */}
         <span>23 pizzas</span>
         <span>$23.45</span>
       </p>

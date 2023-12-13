d269d3be45def43bb392d91922cf127afc9b9616
natnael772 <natnaeldeyas0@gmail.com>
2023-12-13 10:55:57 +0300

    feat: add functions for getting cart and current cart item quantity

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -47,4 +47,9 @@ export const getTotalCartPrice = (state) =>
 export const getTotalCartQuantity = (state) =>
   state.cart.cart.reduce((sum, item) => sum + item.quantity, 0);
 
+export const getCart = (state) => state.cart.cart;
+
+export const getCurrentQuantityById = (id) => (state) =>
+  state.cart.cart.find((item) => item.pizzaId === id)?.quantity ?? 0;
+
 export default cartSlice.reducer;

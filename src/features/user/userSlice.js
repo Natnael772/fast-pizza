a5d588f1b3b6ef47742acf257834e0bcbba64435
natnael772 <natnaeldeyas0@gmail.com>
2023-12-13 16:15:14 +0300

    feat: Add async thunk for fetching address based on the geolocation

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,10 +1,35 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { getAddress } from "../../services/apiGeocoding";
 
 function getPosition() {
   return new Promise(function (resolve, reject) {
     navigator.geolocation.getCurrentPosition(resolve, reject);
   });
 }
+
+// Action creator function(asyncthunk) for fetching address based on given coordinates
+export const fetchAddress = createAsyncThunk(
+  "user/fetchAddress",
+  async function () {
+    // 1) Get the user's geolocation position
+    const positionObj = await getPosition();
+    const position = {
+      latitude: positionObj.coords.latitude,
+      longitude: positionObj.coords.longitude,
+    };
+
+    // 2) Reverse geocoding API to get a description of the user's address, so we can display it the order form, so that the user can correct it if wrong
+    const addressObj = await getAddress(position);
+    const address = `${addressObj?.locality}, ${addressObj?.city} ${addressObj?.postcode}, ${addressObj?.countryName}`;
+
+    console.log(addressObj);
+    console.log(address);
+
+    // 3) Return an object with the data needed
+    //Payload of the fulfilled state
+    return { position, address };
+  },
+);
 
 const initialState = {
   username: "",

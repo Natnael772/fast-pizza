f99b7adce4c567701a5441da7f488db96d7d3ac9
natnael772 <natnaeldeyas0@gmail.com>
2023-12-13 14:54:53 +0300

    Display total cart items quantity and price

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -1,13 +1,21 @@
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { getTotalCartPrice, getTotalCartQuantity } from "./cartSlice";
+import { formatCurrency } from "../../utils/helpers";
 // div  text-stone-200
 
 function CartOverview() {
+  const totalCartQuantity = useSelector(getTotalCartQuantity);
+  const totalCartPrice = useSelector(getTotalCartPrice);
+
+  if (!totalCartQuantity) return null;
+
   return (
     <div className="flex items-center justify-between gap-10 bg-stone-800 p-4 text-sm uppercase text-stone-200 sm:px-6 md:text-base">
       <p className="space-x-4 font-semibold text-stone-300 sm:space-x-6 md:text-base ">
         {/* <p className="space-x-4 font-semibold text-orange-900 sm:text-green-500  md:text-yellow-500 lg:text-red-500"> */}
-        <span>23 pizzas</span>
-        <span>$23.45</span>
+        <span>{totalCartQuantity} pizzas</span>
+        <span>{formatCurrency(totalCartPrice)}</span>
       </p>
       {/* <a href="#">Open cart &rarr;</a> */}
       <Link to="/cart">Open cart &rarr;</Link>

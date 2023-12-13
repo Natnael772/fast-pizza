92899952e9a58a051767a6790f5f6c4337f56f70
natnael772 <natnaeldeyas0@gmail.com>
2023-12-13 14:52:33 +0300

    Add Delete button for deleting cart item

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -1,8 +1,11 @@
-import Button from "../../ui/Button";
+import { useDispatch } from "react-redux";
+// import Button from "../../ui/Button";
 import { formatCurrency } from "../../utils/helpers";
+import { deleteItem } from "./cartSlice";
+import DeleteItem from "./DeleteItem";
 
 function CartItem({ item }) {
-  const { name, quantity, totalPrice } = item;
+  const { pizzaId, name, quantity, totalPrice } = item;
 
   return (
     <li className="py-3 sm:flex sm:items-center sm:justify-between">
@@ -11,7 +14,11 @@ function CartItem({ item }) {
       </p>
       <div className="flex items-center justify-between sm:gap-6">
         <p className="text-sm font-bold">{formatCurrency(totalPrice)}</p>
-        <Button type="secondary">Delete</Button>
+        {/* <Button type="secondary" onClick={handleDeleteItemFromCart}>
+          Delete
+        </Button> */}
+
+        <DeleteItem pizzaId={pizzaId} />
       </div>
     </li>
   );

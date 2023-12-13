679d2f3632e4edee86d23add7ea6b26e7d85a7ff
natnael772 <natnaeldeyas0@gmail.com>
2023-12-13 10:52:11 +0300

    feat: add reducer for increasing item quantity

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -14,6 +14,18 @@ const cartSlice = createSlice({
     deleteItem(state, action) {
       state.cart = state.cart.filter((item) => item.pizzaId != action.payload);
     },
+    increaseItemQuantity(state, action) {
+      const item = state.cart.find((item) => item.pizzaId == action.payload);
+      item.quantity++;
+      item.totalPrice = item.quantity * item.unitPrice;
+
+      //remove if its quantity is 0
+      if (item.quantity === 0) cartSlice.caseReducers.deleteItem(state, action);
+      //  other way:
+      // state.cart = state.cart.filter(
+      // (item) => item.pizzaId != action.payload,
+      // );
+    },
   },
 });
 

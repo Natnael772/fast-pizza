c9e32c058dc3ce4942c48dd99ea69d8234ef8afc
natnael772 <natnaeldeyas0@gmail.com>
2023-12-13 15:19:41 +0300

    feat: Get current position of user

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,5 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+function getPosition() {
+  return new Promise(function (resolve, reject) {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+}
+
 const initialState = {
   username: "",
   status: "idle",
